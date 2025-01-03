195c202b9228d92685eb3c71534076f23b52b307
evan-miller-jumpmind <70151986+evan-miller-jumpmind@users.noreply.github.com>
2025-01-03 07:39:14 -0500

    Merged 3.15 into 3.16

--- symmetric-io/src/main/java/org/jumpmind/symmetric/io/data/writer/AbstractDatabaseWriter.java
+++ symmetric-io/src/main/java/org/jumpmind/symmetric/io/data/writer/AbstractDatabaseWriter.java
@@ -421,8 +421,6 @@ abstract public class AbstractDatabaseWriter implements IDataWriter {
     
     protected abstract void logFailureDetails(Throwable e, CsvData data, boolean logLastDmlDetails, Object[] values);
 
-    protected abstract void logFailureDetails(Throwable e, CsvData data, boolean logLastDmlDetails, Object[] values);
-
     /***
      * Parses source data and returns string array with values in same positions as target columns.
      * 
