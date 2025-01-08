e26675aee0add61bd59322e4c7e6b899f0c6880f
Philip Marzullo <pmarzullo@jumpmind.com>
2025-01-08 13:03:27 -0500

    6690: Code formatting

--- symmetric-db/src/main/java/org/jumpmind/db/alter/RemoveTriggerChange.java
+++ symmetric-db/src/main/java/org/jumpmind/db/alter/RemoveTriggerChange.java
@@ -6,17 +6,16 @@ import org.jumpmind.db.model.Trigger;
 
 public class RemoveTriggerChange extends TableChangeImplBase {
     private Trigger _trigger;
-    //private 
-    
+
     public RemoveTriggerChange(Table table, Trigger trigger) {
         super(table);
         this._trigger = trigger;
     }
-    
+
     public Trigger getTrigger() {
         return _trigger;
     }
-    
+
     @Override
     public void apply(Database database, boolean caseSensitive) {
         Table table = database.findTable(getChangedTable().getName(), caseSensitive);
